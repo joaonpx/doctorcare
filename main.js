2ecf094de8f50e7119bc2f0f1875f5b8cd15c027
Chey <cheyprivado@gmail.com>
2022-05-08 22:54:14 -0300

    quinta aula finalizada :rocket:

--- main.js
+++ main.js
@@ -4,6 +4,55 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+
+  // topo da seção
+  const sectionTop = section.offsetTop
+
+  // altura da seção
+  const sectionHeight = section.offsetHeight
+
+  // o topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  // verificar se a base está abaixo da linha alvo
+  // quais dados vou precisar?
+
+  // a seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // o final da seção passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoudaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  // procurando o atributo id dentro da seção
+  const sectionId = section.getAttribute('id')
+
+  // pegando o atributo id da seção como string
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+
+  if (sectionBoudaries) {
+    menuElement.classList.add('active')
+
+    console.log(`seção atual: ${sectionId} \nfoi adicionada a classe active!`)
+  }
 }
 
 function showNavOnScroll() {
